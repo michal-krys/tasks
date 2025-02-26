bbf0a84ed9bbf78b36f8ff8099547019b3926404
Michal Kryszewski <m.kryszewski@gmail.com>
2025-02-26 20:10:05 +0100

    18.2 crud-tasks Added

--- src/main/java/com/crud/tasks/domain/Task.java
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -1,12 +1,22 @@
 package com.crud.tasks.domain;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
+@Entity(name = "tasks")
 public class Task {
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @Column(name = "name")
     private String title;
+
+    @Column(name = "description")
     private String content;
 }
